c46212d000de7a1e8487503e24f56fc7e9ae98fe
Artsiom Hardziyonak <arrtemio@gmail.com>
2023-11-09 17:02:05 +0300

    store changes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { tasksReducer } from '../entities/Task/model/slice/tasksSlice';
-// ...
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+
+});
 
 export const store = configureStore({
-    reducer: {
-        tasks: tasksReducer,
-    },
+    reducer: rootReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>
