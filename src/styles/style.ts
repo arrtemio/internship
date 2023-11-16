55c56d67a62985ac6619c9d7c70a525abe0ad382
Artsiom Hardziyonak <arrtemio@gmail.com>
2023-11-16 17:15:12 +0300

    sx styles for mui components

---
+++ src/styles/style.ts
@@ -0,0 +1,12 @@
+export const flexColumn = {
+    display: 'flex',
+    flexDirection: 'column',
+};
+
+export const cardStyle = {
+    width: '100%',
+    display: 'flex',
+    padding: '10px',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+};
