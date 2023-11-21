ecf15752c2a729c32accc6e1e61cf98379b0847e
Artsiom Hardziyonak <arrtemio@gmail.com>
2023-11-21 13:18:22 +0300

    The colors variant retrieval has been moved to an object, and the function returnStatusColor with its tests have been removed

--- src/shared/lib/helpers/returnStatusColor/returnStatusColor.test.ts
+++
@@ -1,6 +0,0 @@
-import { Status } from 'entities/Task';
-import { returnStatusColor } from './returnStatusColor';
-
-test('should return success', () => {
-    expect(returnStatusColor(Status.COMPLETED)).toEqual('success');
-});
--- src/styles/style.ts
+++ src/styles/style.ts
@@ -5,20 +5,20 @@ export const flexColumn = {
     flexDirection: 'column',
 };
 
-export const flexBetween = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-};
-
-export const cardStyle = {
-    width: '100%',
-    padding: '10px',
-    ...flexBetween,
-};
-
 export const statusColors = {
     [Status.COMPLETED]: '#2e7d32',
     [Status.IN_PROGRESS]: '#9c27b0',
     [Status.TO_DO]: '#1976d2',
+};
+
+type ColorsVariantType = {
+    'Completed': 'success',
+    'In progress': 'secondary',
+    'To do': 'primary',
+};
+
+export const colorsVariant: ColorsVariantType = {
+    [Status.COMPLETED]: 'success',
+    [Status.IN_PROGRESS]: 'secondary',
+    [Status.TO_DO]: 'primary',
 };
