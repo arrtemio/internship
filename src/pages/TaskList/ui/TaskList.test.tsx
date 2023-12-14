0d309e62b114c45b54648e2ed2bd04bfd2ba35ba
Artsiom Hardziyonak <arrtemio@gmail.com>
2023-12-14 12:15:39 +0300

    TaskList test changes

--- src/pages/TaskList/ui/TaskList.test.tsx
+++ src/pages/TaskList/ui/TaskList.test.tsx
@@ -1,98 +1,41 @@
-import { generateRandomId, getDateAndTime } from 'shared/lib/helpers';
-import { screen, fireEvent, waitFor } from '@testing-library/react';
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
+import { screen, waitFor } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { testTask } from 'shared/test/TestTask';
-import { createTask, getTasksData, Status } from 'entities/Task';
-import { act } from 'react-dom/test-utils';
-import userEvent from '@testing-library/user-event';
+import { firestore } from 'app/firebase';
+import 'core-js';
+import '@testing-library/jest-dom';
 import { componentRender } from 'shared/lib/tests';
-import { getAllTasks } from 'entities/Task/model/actions/tasksActions';
-import { useAppDispatch, useAppSelector } from 'shared/lib/hooks/redux';
+import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
 import { TaskList } from './TaskList';
-import 'app/firebase/index';
 
-jest.mock('shared/lib/helpers');
-jest.mock('entities/Task/model/actions/tasksActions');
-jest.mock('shared/lib/hooks/redux');
+const middlewares = [thunk.withExtraArgument({ firestore })];
+const mockStore = configureStore(middlewares);
 
-describe('TaskList test', () => {
-    test('', () => {
-        expect(2 + 2).toEqual(4);
+describe('TaskList', () => {
+    it('renders the task list correctly', async () => {
+        const initialState = {
+            tasks: {
+                data: [testTask],
+                isLoading: false,
+                error: undefined,
+            },
+        };
+        type StoreType = {
+            dispatch: ThunkDispatch<StoreType, void, AnyAction>;
+        };
+
+        const store: StoreType = mockStore(initialState);
+        const renderTaskList = () => componentRender(<TaskList />, { initialState });
+
+        jest.spyOn(store, 'dispatch').mockImplementationOnce(() => Promise.resolve());
+
+        renderTaskList();
+
+        await waitFor(() => {
+            const title = screen.getByTestId('TaskCard-title');
+            expect(title).toHaveTextContent(testTask.title);
+        });
     });
-    // const renderTaskList = () => componentRender(<TaskList />, {
-    //     initialState: {},
-    // });
-    // const dateAndTime = '14.11.2023 / 15.53.23';
-    // const mockedDispatch = jest.fn();
-    //
-    // beforeEach(() => {
-    //     (generateRandomId as jest.Mock).mockImplementation(() => 'ghj43GF6');
-    //     (getDateAndTime as jest.Mock).mockImplementation(() => dateAndTime);
-    // });
-    //
-    // test('Create task test', async () => {
-    //     (useAppSelector as jest.Mock).mockReturnValue({ data: [], isLoading: false, error: undefined });
-    //     (useAppDispatch as jest.Mock).mockReturnValue(mockedDispatch);
-    //
-    //     renderTaskList();
-    //     expect(getAllTasks).toHaveBeenCalled();
-    //     screen.debug();
-    //
-    //     const taskInput = (screen.getByLabelText(/create new task/i));
-    //     fireEvent.input(taskInput, { target: { value: 'New task' } });
-    //     fireEvent.click(screen.getByTestId('AddTask-button'));
-    //
-    //     await waitFor(() => {
-    //         expect(screen.getByText(`Created:${dateAndTime}`)).toBeInTheDocument();
-    //         expect(screen.getByText('New task')).toBeInTheDocument();
-    //     });
-    // });
-    //
-    // test('Create subtask test', () => {
-    //     (useAppSelector as jest.Mock).mockReturnValue([{ ...testTask, subTasks: [] }]);
-    //     renderTaskList();
-    //
-    //     fireEvent.click(screen.getByText(testTask.title));
-    //
-    //     const subTaskInput = screen.getByLabelText(/create sub task/i);
-    //     fireEvent.input(subTaskInput, { target: { value: 'New_test_sub_task' } });
-    //     fireEvent.click(screen.getByTestId(`AddTask-button-${testTask.id}`));
-    //
-    //     expect(generateRandomId).toHaveBeenCalled();
-    //     expect(screen.getByText('New_test_sub_task')).toBeInTheDocument();
-    // });
-    //
-    // test('Create new sub task, when task is completed', () => {
-    //     (useAppSelector as jest.Mock).mockReturnValue([{ ...testTask, status: Status.COMPLETED, subTasks: [] }]);
-    //     (useAppDispatch as jest.Mock).mockReturnValue(mockedDispatch);
-    //     renderTaskList();
-    //
-    //     fireEvent.click(screen.getByText('Click for more'));
-    //     fireEvent.input(screen.getByLabelText(/create sub task/i), { target: { value: 'New_test_sub_task' } });
-    //     fireEvent.click(screen.getByTestId(`AddTask-button-${testTask.id}`));
-    //
-    //     expect(generateRandomId).toHaveBeenCalled();
-    //     expect(screen.getByText('New_test_sub_task')).toBeInTheDocument();
-    //     expect(screen.queryAllByText(Status.COMPLETED)).toHaveLength(0);
-    //     expect(screen.getAllByText(Status.IN_PROGRESS)).toHaveLength(1);
-    // });
-    //
-    // test('Change task status to Completed. Task and subtask status should be Completed', async () => {
-    //     (useAppSelector as jest.Mock).mockReturnValue([testTask]);
-    //     (useAppDispatch as jest.Mock).mockReturnValue(mockedDispatch);
-    //     renderTaskList();
-    //
-    //     fireEvent.click(screen.getByText(testTask.title));
-    //     act(() => {
-    //         userEvent.click(screen.getByText(testTask.status));
-    //     });
-    //     act(() => {
-    //         userEvent.click(screen.getByText(Status.COMPLETED));
-    //     });
-    //
-    //     await waitFor(() => {
-    //         expect(screen.getAllByText(Status.COMPLETED)).toHaveLength(testTask.subTasks.length + 1);
-    //     });
-    // });
 });
